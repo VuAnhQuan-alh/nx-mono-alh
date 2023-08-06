9b20dc2d733877c0b948608e152c2818a1703cd9
VuAnhQuan-alh <quanva2521@gmail.com>
2023-08-06 13:16:34 +0700

    ahi oke

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const port = process.env.PORT || 1520;
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Api Gateway is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+bootstrap();
